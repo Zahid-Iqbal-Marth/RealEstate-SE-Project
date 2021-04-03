8afb7660437d6790cc6d1077bb29dcd0d5e5ab33
Zahid-Iqbal-Marth <61237787+Zahid-Iqbal-Marth@users.noreply.github.com>
2021-04-03 11:15:13 +0500

    Update settings.py

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -144,13 +144,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'zi090545@gmail.com'
-EMAIL_HOST_PASSWORD = 'vzjhnysawjhyitzr'
+EMAIL_HOST_USER = '<your email>'
+EMAIL_HOST_PASSWORD = '<your password>'
 
 
 
 #payment
 
-STRIPE_SECRET_KEY = 'sk_test_Q36Vbp7MKhTMAZ8JBRkoSg5e00hwrikSwq'
-STRIPE_PUBLISHABLE_KEY = 'pk_test_za67VN5MomArCvewcHW1XZ8f00yfsImUOy'
+STRIPE_SECRET_KEY = '<enter your stripe private key>'
+STRIPE_PUBLISHABLE_KEY = '<enter your stripe public key>'
 
